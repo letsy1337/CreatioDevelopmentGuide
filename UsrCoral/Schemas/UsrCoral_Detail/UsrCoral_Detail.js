d0b89ee4d5c54d7e9eb1028067b407537890e05f
letsy1337 <skynightandday@gmail.com>
2025-08-13 13:57:26 +0300

    Created section "Coral" + vw of age in days

---
+++ UsrCoral/Schemas/UsrCoral_Detail/UsrCoral_Detail.js
@@ -0,0 +1,8 @@
+define('UsrCoral_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrCoral',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
---
+++ UsrCoral/SqlScripts/DeleteBlueTrafficLightMSSQL/DeleteBlueTrafficLightMSSQL.sql
@@ -0,0 +1,2 @@
+delete from UsrTrafficLight
+where Id = 'cbc511f3-143f-4056-8129-332401288698'
